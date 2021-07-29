b8ffed4b912fe867bbb853f8ee4ea889fee9de86
mdharb <md20.harb21@gmail.com>
2021-07-29 15:12:00 +0300

    pushing profile

--- src/views/examples/Profile.js
+++ src/views/examples/Profile.js
@@ -124,31 +124,28 @@ class Profile extends React.Component {
                   </Row>
                   <div className="text-center mt-5">
                     <h3>
-                      Jessica Jones{" "}
-                      <span className="font-weight-light">, 27</span>
+                      Mohammad Harb{" "}
+                      <span className="font-weight-light">, 28</span>
                     </h3>
                     <div className="h6 font-weight-300">
                       <i className="ni location_pin mr-2" />
-                      Bucharest, Romania
+                      Amman, Jordan
                     </div>
                     <div className="h6 mt-4">
                       <i className="ni business_briefcase-24 mr-2" />
-                      Solution Manager - Creative Tim Officer
+                      Software Developer | Industrial Engineer
                     </div>
                     <div>
                       <i className="ni education_hat mr-2" />
-                      University of Computer Science
+                      Jordan University of Science & Technology
                     </div>
                   </div>
                   <div className="mt-5 py-5 border-top text-center">
                     <Row className="justify-content-center">
                       <Col lg="9">
                         <p>
-                          An artist of considerable range, Ryan — the name taken
-                          by Melbourne-raised, Brooklyn-based Nick Murphy —
-                          writes, performs and records all of his own music,
-                          giving it a warm, intimate feel with a solid groove
-                          structure. An artist of considerable range.
+                        Holding a bachelor’s degree in industrial engineering from Jordan university of science and technology. Currently following the passion of being a well-educated and skilled software developer by enrolling in an intensive course in software development presented
+                        by ASAC in collaboration with Code Fellows. Moreover experienced in the logistics with more than a year and a half of experience. Having a demonstrated history working and volunteering in the humanitarian sector locally and abroad.
                         </p>
                         <a href="#pablo" onClick={e => e.preventDefault()}>
                           Show more
